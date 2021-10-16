bfb5b59229fb3242aa62d2f6e3533168608d8736
임동혁 <as0387@naver.com>
2021-10-16 21:21:21 +0900

    소켓채팅 테스트 전

--- src/main/java/com/dongs/jwt/config/WebConfig.java
+++ src/main/java/com/dongs/jwt/config/WebConfig.java
@@ -8,34 +8,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Value("file:///C:/Users/ImD/Desktop/졸업작품/final_itsmine_prj/it'smine_final_backend/ItsminePrj_final/src/main/resources/upload/")
-	private String uploadFolderPath;
-
-	@Value("/upload/**")
-	private String getUploadPath;
-
-	@Value("file:///C:/Users/ImD/Desktop/졸업작품/final_itsmine_prj/it'smine_final_backend/ItsminePrj_final/src/main/resources/upload/public/")
-	private String publicFolderPath;
-
-	@Value("/upload/public/**")
-	private String getPublicPath;
-
-	@Value("file:///C:/Users/ImD/Desktop/졸업작품/final_itsmine_prj/it'smine_final_backend/ItsminePrj_final/src/main/resources/upload/profile/")
-	private String profileFolderPath;
-
-	@Value("/upload/profile/**")
-	private String getProfilePath;
+//	@Value("file:///C:/Users/ImD/Desktop/졸업작품/final_itsmine_prj/it'smine_final_backend/ItsminePrj_final/src/main/resources/upload/")
+//	private String uploadFolderPath;
+//
+//	@Value("/upload/**")
+//	private String getUploadPath;
+//
+//	@Value("file:///C:/Users/ImD/Desktop/졸업작품/final_itsmine_prj/it'smine_final_backend/ItsminePrj_final/src/main/resources/upload/public/")
+//	private String publicFolderPath;
+//
+//	@Value("/upload/public/**")
+//	private String getPublicPath;
+//
+//	@Value("file:///C:/Users/ImD/Desktop/졸업작품/final_itsmine_prj/it'smine_final_backend/ItsminePrj_final/src/main/resources/upload/profile/")
+//	private String profileFolderPath;
+//
+//	@Value("/upload/profile/**")
+//	private String getProfilePath;
 
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	   public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-		if (getUploadPath != null) {
-			registry.addResourceHandler(getUploadPath).addResourceLocations(uploadFolderPath);
-		} else if (getPublicPath != null) {
-			registry.addResourceHandler(getPublicPath).addResourceLocations(publicFolderPath);
-		} else {
-			registry.addResourceHandler(getProfilePath).addResourceLocations(profileFolderPath);
-		}
+	         registry.addResourceHandler("/upload/**").addResourceLocations("file:src/main/resources/upload/");
+	         registry.addResourceHandler("/upload/public/**").addResourceLocations("file:src/main/resources/upload/public/");
+	         registry.addResourceHandler("/upload/profile/**").addResourceLocations("file:src/main/resources/upload/profile/");
 
-	}
+	   }
 }
